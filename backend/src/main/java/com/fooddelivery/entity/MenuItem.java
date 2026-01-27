8c535123ae2ae731cb087e547f3f6f915daaf78c
JacksonSmith43 <jessica444@gmx.at>
2026-01-27 15:09:02 +0100

    Added Entity and Repository for MenuItem.

---
+++ backend/src/main/java/com/fooddelivery/entity/MenuItem.java
@@ -0,0 +1,82 @@
+package com.fooddelivery.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "menu_items")
+public class MenuItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "food_name")
+    private String name;
+
+    private String description;
+    private Double price;
+
+    @Column(name = "food_image")
+    private String imageName;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id")
+    private Restaurants restaurant;
+
+    public MenuItem() {
+
+    }
+
+    public MenuItem(Long id, String name, String description, Double price, String imageName) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imageName = imageName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+
+    @Override
+    public String toString() {
+        return "MenuItem [id=" + id + ", name=" + name + ", description=" + description + ", price=" + price
+                + ", imageName=" + imageName + "]";
+    }
+}
