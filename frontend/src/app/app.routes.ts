7ab7b414e700018def09e0b47ff7e730364cad23
JacksonSmith43 <jessica444@gmx.at>
2026-02-05 15:12:04 +0100

    Added registration without the logic.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,11 +4,15 @@ import { RestaurantDetailComponent } from './restaurants/components/restaurant-d
 import { RestaurantComponent } from './restaurants/components/restaurant.component';
 import { CartComponent } from './cart/cart.component/cart.component';
 import { SearchRestaurant } from './search-restaurant/search-restaurant.component';
+import { RegisterComponent } from './auth/register/register-component/register-component';
 
 export const routes: Routes = [
   // { path: '' LoginComponent},
   // { path: '/login' LoginComponent}
   { path: '', component: SearchRestaurant },
+
+  { path: 'register', component: RegisterComponent },
+
   { path: 'allRestaurants', component: RestaurantComponent },
   { path: 'restaurant/:restaurantName', component: RestaurantDetailComponent },
   { path: 'cart', component: CartComponent },
---
+++ frontend/src/app/auth/register/register-component/register-component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+
+@Component({
+  selector: 'app-register-component',
+  imports: [MatButton],
+  templateUrl: './register-component.html',
+  styleUrl: './register-component.css',
+})
+export class RegisterComponent {}
