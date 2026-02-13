c584fb238c8aa0e92fabc21042c1a7e56b54c689
JacksonSmith43 <jessica444@gmx.at>
2026-02-13 09:39:49 +0100

    Added comments about the modal.

--- frontend/src/app/account/profile/modal/profile-modal/profile-modal.component.ts
+++ frontend/src/app/account/profile/modal/profile-modal/profile-modal.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { MatAnchor } from "@angular/material/button";
+import { MatAnchor } from '@angular/material/button';
 
 import { UserType } from '../../../../auth/model/user-type';
 import { ProfileComponent } from '../../profile.component/profile.component';
@@ -14,10 +14,15 @@ import { ProfileComponent } from '../../profile.component/profile.component';
 })
 export class ProfileModalComponent {
   constructor(
+    // dialogRef references the opened modal. Allows it so that the modal can be closed and that data can be returned.
     public dialogRef: MatDialogRef<ProfileComponent>,
+    // MAT_DIALOG_DATA receives the data (email, password) that it receives from ProfileComponent.
+    // data: UserType is the data that is received.
+    // This is the same as: data = inject<UserType>(MAT_DIALOG_DATA);
     @Inject(MAT_DIALOG_DATA) public data: UserType,
   ) {}
 
+  // Closes the modal without returning anything.
   onNoClick(): void {
     this.dialogRef.close();
   }
--- frontend/src/app/account/profile/profile.component/profile.component.ts
+++ frontend/src/app/account/profile/profile.component/profile.component.ts
@@ -18,27 +18,15 @@ export class ProfileComponent {
   authService = inject(AuthService);
   locaStorage = inject(LocalStorageService);
 
-  constructor(public dialog: MatDialog) {}
-
   email: string = '';
   password: string = '';
-
-  openDialog(): void {
-    const dialogRef = this.dialog.open(ProfileModalComponent, {
-      width: '80%',
-      data: { email: this.email, password: this.password },
-    });
-
-    dialogRef.afterClosed().subscribe((result) => {
-      console.log('The dialog was closed');
-      this.password = result;
-    });
-  }
 
   passwordLength = computed(() => {
     let currentUserPassword = this.authService.currentUser()?.password;
     return currentUserPassword ? currentUserPassword.length : 0;
   });
+
+  constructor(public dialog: MatDialog) {} // MatDialog opens dialogs/modals.
 
   ngOnInit(): void {
     console.log('AccountComponent_ngOnInit().');
@@ -48,6 +36,22 @@ export class ProfileComponent {
     if (userCredentials) {
       this.authService.currentUser.set(userCredentials);
     }
+  }
+
+  openDialog(): void {
+    // ProfileModalComponent is the component that should be displayed when clicking on the modal field.
+    const dialogRef = this.dialog.open(ProfileModalComponent, {
+      width: '80%',
+      data: { email: this.email, password: this.password },
+    });
+
+    // Returns an observable when the user closes the modal. 
+    // result hold the data that the modal returns after it closes.
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+      // The returned result will be saved as a new password.
+      this.password = result;
+    });
   }
 
   getPasswordLength() {
