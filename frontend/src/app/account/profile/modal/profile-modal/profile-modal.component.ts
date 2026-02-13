750392e7adeaae29905cc2ce05bfecb7280cceb2
JacksonSmith43 <jessica444@gmx.at>
2026-02-13 12:29:25 +0100

    Form validation for the email change in modal works. Just not whether the email already exists or is currently in use.

--- frontend/src/app/account/profile/modal/profile-modal/profile-modal.component.ts
+++ frontend/src/app/account/profile/modal/profile-modal/profile-modal.component.ts
@@ -1,6 +1,7 @@
-import { Component, inject, Inject } from '@angular/core';
+import { Component, inject, Inject, signal } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { MatAnchor } from '@angular/material/button';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { UserType } from '../../../../auth/model/user-type';
 import { ProfileComponent } from '../../profile.component/profile.component';
@@ -9,13 +10,20 @@ import { AccountService } from '../../../service/account.service';
 @Component({
   selector: 'app-profile-modal',
   standalone: true,
-  imports: [MatAnchor],
+  imports: [MatAnchor, ReactiveFormsModule],
   templateUrl: './profile-modal.component.html',
   styleUrl: './profile-modal.component.css',
 })
 export class ProfileModalComponent {
   accountService = inject(AccountService);
+
+  successMessage = signal<string>('');
+
   selectedFormField = this.accountService.selectedFormField;
+
+  emailForm = new FormGroup({
+    email: new FormControl('', [Validators.email, Validators.required]),
+  });
 
   constructor(
     // dialogRef references the opened modal. Allows it so that the modal can be closed and that data can be returned.
@@ -34,11 +42,35 @@ export class ProfileModalComponent {
 
   onChangeEmail() {
     console.log('onChangeEmail().');
-    this.selectedFormField.set('email');
+
+    if (this.emailForm.valid) {
+      let email = this.emailForm.value.email;
+      this.selectedFormField.set('email');
+      this.successMessage.set('Your Email address has successfully been changed.');
+
+      setTimeout(() => {
+        this.dialogRef.close(email);
+        this.successMessage.set('');
+      }, 2000);
+
+      console.log('onChangeEmail()_email: ', email);
+    }
   }
 
   onChangePassword() {
     console.log('onChangePassword().');
     this.selectedFormField.set('password');
+  }
+
+  get emailIsInvalid() {
+    console.log('emailIsInvalid().');
+
+    if (this.emailForm.controls.email.hasError('required')) {
+      return 'An input is required.';
+    } else if (this.emailForm.controls.email.hasError('email')) {
+      return 'It has to be a valid email address.';
+    } else {
+      return '';
+    }
   }
 }
