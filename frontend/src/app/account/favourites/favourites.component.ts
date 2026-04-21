0a7efd75bf0f66ab3b2d3b3405ac57077cc530ab
JacksonSmith43 <jessica444@gmx.at>
2026-04-21 10:30:17 +0200

    Added auth checks to favourites and my-addresses.

--- frontend/src/app/account/favourites/favourites.component.ts
+++ frontend/src/app/account/favourites/favourites.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+
+import { FavouritesService } from '../../shared/services/favourites.service';
+import { LocalStorageService } from '../../shared/services/local-storage.service';
+import { AuthService } from '../../auth/service/auth.service';
+import { AccountService } from '../service/account.service';
 
 @Component({
   selector: 'app-favourites',
@@ -6,6 +11,29 @@ import { Component } from '@angular/core';
   templateUrl: './favourites.component.html',
   styleUrl: './favourites.component.css',
 })
-export class FavouritesComponent {
+export class FavouritesComponent implements OnInit {
+  favouritesService = inject(FavouritesService);
+  locaStorageService = inject(LocalStorageService);
+  authService = inject(AuthService);
+  accountService = inject(AccountService);
 
+  ngOnInit(): void {
+    console.log('FavouritesComponent_ngOnInit().');
+
+    let userCredentials = this.locaStorageService.getUserCredentials();
+
+    if (userCredentials) {
+      this.authService.authUser.set(userCredentials);
+
+      this.accountService.getUserProfile(userCredentials.email).subscribe({
+        next: (user) => {
+          console.log('FavouritesComponent_ngOnInit()_user: ', user);
+          this.accountService.currentUserProfile.set(JSON.parse(user));
+        },
+        error: (error) => {
+          console.error('FavouritesComponent_ngOnInit()_Error loading profile: ', error.error.message);
+        },
+      });
+    }
+  }
 }
